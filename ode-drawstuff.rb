65b4b30c68a87bddbbd3625eea3bab3075a67614
SANO Akira <sanoakr@gmail.com>
2014-12-18 14:06:07 +0900

    1st test version

--- ode-drawstuff.rb
+++ ode-drawstuff.rb
@@ -16,28 +16,29 @@ class OdeDrawstuff < Formula
 
   def options
     [
-      ["--enable-double-precision", "Configure ODE to work with double precision"],
-      ["--enable-libccd", "Enable all libccd colliders (except box-cylinder)"],
+      ["--disable-double-precision", "Configure ODE to work without double precision"],
+      ["--disable-libccd", "Disable all libccd colliders (except box-cylinder)"],
+      ["--enable-demos", "Build and install Drawstuff"],
       ["--with-drawstuff", "Build and install Drawstuff"]
     ]
   end
 
   def install
     args = []
-    args << "--disable-demos"
-    args << "--enable-double-precision" if ARGV.include? "--enable-double-precision"
-    args << "--enable-libccd" if ARGV.include? "--enable-libccd"
+    args << "--disable-demos" unless ARGV.include? "--enable-demos"
+    args << "--enable-double-precision" unless ARGV.include? "--disable-double-precision"
+    args << "--enable-libccd" unless ARGV.include? "--disable-libccd"
 
     if ARGV.build_head?
       ENV['LIBTOOLIZE'] = 'glibtoolize'
-      inreplace 'autogen.sh', 'libtoolize', '$LIBTOOLIZE'
-      system "./autogen.sh"
+      inreplace 'boostrap', 'libtoolize', '$LIBTOOLIZE'
+      system "./bootstrap"
     end
     system "./configure", "--prefix=#{prefix}", *args
     system "make"
     system "make install"
     if(ARGV.include?("--with-drawstuff"))
-      system "curl -O https://raw.github.com/gist/3225621/install_drawstuff.sh"
+      system "curl -O https://raw.githubusercontent.com/sanoakr/homebrew-slab/master/install_drawstaff.sh"
       system "chmod a+x install_drawstuff.sh"
       system "./install_drawstuff.sh"
     end
